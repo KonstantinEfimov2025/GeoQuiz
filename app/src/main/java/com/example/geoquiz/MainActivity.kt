f58dc670344a520c23445d6e29112d90e96986d6
konstantin <konstantinefimov3742@gmail.com>
2025-10-27 18:37:29 +0700

    Question struct

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.geoquiz.ui.theme.GeoQuizTheme
 
@@ -20,10 +24,22 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Question(
+    val text: String,
+    val answer: Boolean
+)
+
 @Composable
 fun GeoQuiz() {
+    val questions = listOf(
+        Question("Canberra is the capital of Australia.", true),
+        Question("The Pacific Ocean is larger than the Atlantic Ocean.", true)
+    )
+
+    var indexQuestion by remember { mutableStateOf(0) }
+
     Column {
-        Text(text = "GeoQuiz")
+        Text(text = questions[indexQuestion].text)
     }
 }
 
